01beb80e264edef562a58e186bf8ba5de237fb0d
CmdrSheath <75804652+CmdrSheath@users.noreply.github.com>
2026-02-27 20:35:25 -0500

    Implement dungeon functions and structure

--- src/dungeon.c
+++ src/dungeon.c
@@ -1,1 +1,8 @@
 /////
+// Dungeon/first-person view - implement later
+#include "game.h"
+void initDungeon() {}
+void updateDungeon() {}
+void cleanupDungeon() {}
+void renderDungeonView() {}
+void moveDungeon(u8 direction) {}
