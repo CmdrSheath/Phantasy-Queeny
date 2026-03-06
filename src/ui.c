8b53014ad45893cc4d2fac31eb277811cb92a40f
CmdrSheath <75804652+CmdrSheath@users.noreply.github.com>
2026-03-06 06:30:42 -0500

    Update ui.c

--- src/ui.c
+++ src/ui.c
@@ -22,7 +22,6 @@ void drawText(u16 x, u16 y, const char* text) {
 
 void drawNumber(u16 x, u16 y, u16 number, u8 digits) {
     char buffer[6];
-    // Simple number to string
     switch(digits) {
         case 1: sprintf(buffer, "%1d", number); break;
         case 2: sprintf(buffer, "%2d", number); break;
@@ -35,20 +34,20 @@ void drawNumber(u16 x, u16 y, u16 number, u8 digits) {
 }
 
 void showMessage(const char* text) {
-    // Clear previous message area
-    VDP_clearTextArea(2, 24, 36, 2);
+    // Clear previous message area on BG_B
+    VDP_clearTextAreaBG(BG_B, 2, 24, 36, 2);
     
     // Copy and display new message
     strncpy(currentMessage, text, 39);
     currentMessage[39] = '\0';
-    VDP_drawText(currentMessage, 2, 24);
+    VDP_drawTextBG(BG_B, currentMessage, 2, 24);
     
     // Set timer (messages auto-clear after 3 seconds)
-    messageTimer = 180; // 3 seconds at 60fps
+    messageTimer = 180;
 }
 
 void clearMessage() {
-    VDP_clearTextArea(2, 24, 36, 2);
+    VDP_clearTextAreaBG(BG_B, 2, 24, 36, 2);
     currentMessage[0] = '\0';
     messageTimer = 0;
 }
