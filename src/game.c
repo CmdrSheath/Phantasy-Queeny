3f24ec0bbff9c3b5d5545a5040322bcd1b6c4a9c
CmdrSheath <75804652+CmdrSheath@users.noreply.github.com>
2026-03-10 17:06:24 -0400

    Update game.c

--- src/game.c
+++ src/game.c
@@ -4,9 +4,12 @@
 
 #include "game.h"
 #include <genesis.h>
+#include "audio.h"
 
 void initGameSystem(void) {
     // Global initialization
     // Seed random number generator
     setRandomSeed(getTick());
+    initAudio();
+    playMusic(MUSIC_TITLE);
 }
